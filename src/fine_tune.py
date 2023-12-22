e960e4f104494e378951d39f95aebe677f8b5b7a
WeatherBoy <dafloxi@gmail.com>
2023-12-22 10:00:54 +0100

    :construction: print hyperparameters nicely

--- src/fine_tune.py
+++ src/fine_tune.py
@@ -1,10 +1,11 @@
 import hydra
+from omegaconf import OmegaConf
 from models.fine_tune_llama2 import fine_tune_and_save_model, initialize_model_and_tokenizer, load_data, visualize_and_save
 
 
 @hydra.main(config_path="../conf", config_name="config")
 def main(cfg):
-    print(cfg.pretty())
+    print(OmegaConf.to_yaml(cfg))
 
 
 def fine_tune():
