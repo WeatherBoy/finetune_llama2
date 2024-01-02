f95d5f9d9076c408662f5745a30fae96ededb28e
WeatherBoy <dafloxi@gmail.com>
2024-01-02 16:08:21 +0100

    :recycle: data handling goes to data handling DIR

--- src/data/generate_dataset.py
+++ src/data/generate_dataset.py
@@ -1,6 +1,43 @@
+import pickle
+
+import wandb
+from datasets import load_dataset
 from fine_tune_llama2 import load_data_to_fine_tune, store_dataset_locally
 
 # from fine_tune_llama2 import transform_dataset_format
+
+
+def load_data():
+    """Load the new dataset."""
+    dataset = load_dataset(wandb.config.NEW_DATASET_NAME)
+    return dataset
+
+
+def store_dataset_locally(dataset):
+    """Store the dataset locally using pickle."""
+    with open(wandb.config.NEW_DATASET_NAME_LOCAL, "wb") as file:
+        pickle.dump(dataset, file)
+
+
+def load_dataset_from_local():
+    """Load the dataset from a local directory using pickle."""
+    with open(wandb.config.NEW_DATASET_NAME_LOCAL, "rb") as file:
+        dataset = pickle.load(file)
+    return dataset
+
+
+def transform_dataset_format(df):
+    """Transform the dataframe into a specified format."""
+
+    def transform(row):
+        user_text = row["question"]
+        assistant_text = row["answer"]
+        return f"<s>[INST] <</SYS>>\n{wandb.config.SYSTEM_MESSAGE.strip()}\n<</SYS>>\n\n" f"{user_text} [/INST] {assistant_text} </s>"
+
+    transformed_df = df.apply(transform, axis=1).to_frame(name="text")
+    transformed_df.reset_index(drop=True, inplace=True)
+
+    return transformed_df
 
 
 def generate_dataset():
--- src/models/fine_tune_llama2.py
+++ src/models/fine_tune_llama2.py
@@ -4,14 +4,16 @@ import pickle
 import time
 
 import hydra
-import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 import wandb
 from datasets import Dataset, load_dataset
 from omegaconf import DictConfig, OmegaConf
-from peft import LoraConfig, PeftModel, get_peft_model, prepare_model_for_kbit_training
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, EarlyStoppingCallback, TrainingArguments, pipeline
+from peft import (LoraConfig, PeftModel, get_peft_model,
+                  prepare_model_for_kbit_training)
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          BitsAndBytesConfig, EarlyStoppingCallback,
+                          TrainingArguments, pipeline)
 from trl import SFTTrainer
 
 
@@ -49,39 +51,6 @@ def memory_decorator(func):
         return result, metrics
 
     return wrapper
-
-
-def load_data():
-    """Load the new dataset."""
-    dataset = load_dataset(wandb.config.NEW_DATASET_NAME)
-    return dataset
-
-
-def store_dataset_locally(dataset):
-    """Store the dataset locally using pickle."""
-    with open(wandb.config.NEW_DATASET_NAME_LOCAL, "wb") as file:
-        pickle.dump(dataset, file)
-
-
-def load_dataset_from_local():
-    """Load the dataset from a local directory using pickle."""
-    with open(wandb.config.NEW_DATASET_NAME_LOCAL, "rb") as file:
-        dataset = pickle.load(file)
-    return dataset
-
-
-def transform_dataset_format(df):
-    """Transform the dataframe into a specified format."""
-
-    def transform(row):
-        user_text = row["question"]
-        assistant_text = row["answer"]
-        return f"<s>[INST] <</SYS>>\n{wandb.config.SYSTEM_MESSAGE.strip()}\n<</SYS>>\n\n" f"{user_text} [/INST] {assistant_text} </s>"
-
-    transformed_df = df.apply(transform, axis=1).to_frame(name="text")
-    transformed_df.reset_index(drop=True, inplace=True)
-
-    return transformed_df
 
 
 def initialize_model_and_tokenizer():
