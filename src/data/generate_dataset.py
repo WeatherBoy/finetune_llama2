49ab4f2539240bae55491066b87fddb96c04ffa8
WeatherBoy <dafloxi@gmail.com>
2024-01-09 10:51:34 +0100

    :construction: try to generate data

--- src/data/generate_dataset.py
+++ src/data/generate_dataset.py
@@ -1,8 +1,27 @@
 import pickle
 
 import wandb
+import pandas as pd
 from datasets import load_dataset
 from load_data_to_finetune import load_data_to_fine_tune
+
+
+def load_json_to_dataframe():
+    """Load the json file into a dataframe."""
+    df = pd.read_json("../../data/processed/GPT4_synthetic_data.json")
+
+    def transform(row):
+        user_text = row["documentationInstruction"]
+        assistant_text = row["processedDocumentation"]
+        return (
+            f"<s>[INST] <</SYS>>\nGiven a puzzle-like code question, provide a well-reasoned, step-by-step Python solution.\n<</SYS>>\n\n"
+            f"{user_text} [/INST] {assistant_text} </s>"
+        )
+
+    transformed_df = df.apply(transform, axis=1).to_frame(name="text")
+    transformed_df.reset_index(drop=True, inplace=True)
+    print(transformed_df.head())
+    return df
 
 
 def load_data():
@@ -46,4 +65,4 @@ def generate_dataset():
 
 
 if __name__ == "__main__":
-    generate_dataset()
+    load_json_to_dataframe()
