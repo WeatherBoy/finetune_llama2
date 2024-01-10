4750d7c8cd71d16f58025e79024464f0cdd50e30
WeatherBoy <dafloxi@gmail.com>
2024-01-10 15:23:27 +0100

    :monocle_face: check data is formatted correct

--- src/data/generate_dataset.py
+++ src/data/generate_dataset.py
@@ -8,7 +8,7 @@ from load_data_to_finetune import load_data_to_fine_tune
 
 def load_json_to_dataframe():
     """Load the json file into a dataframe."""
-    df = pd.read_json("../../data/processed/GPT4_synthetic_data.json")
+    df = pd.read_json("../../data/raw/GPT4_synthetic_data5.json")
 
     def transform(row):
         user_text = row["documentationInstruction"]
@@ -20,7 +20,7 @@ def load_json_to_dataframe():
 
     transformed_df = df.apply(transform, axis=1).to_frame(name="text")
     transformed_df.reset_index(drop=True, inplace=True)
-    print(transformed_df.head())
+
     return df
 
 
@@ -30,15 +30,17 @@ def load_data():
     return dataset
 
 
-def store_dataset_locally(dataset):
+def store_dataset_locally(dataset, filename: str | None = None) -> None:
     """Store the dataset locally using pickle."""
-    with open(wandb.config.NEW_DATASET_NAME_LOCAL, "wb") as file:
+    filename = wandb.config.NEW_DATASET_NAME_LOCAL if filename is None else filename
+    with open(filename, "wb") as file:
         pickle.dump(dataset, file)
 
 
-def load_dataset_from_local():
+def load_dataset_from_local(filename: str | None = None):
     """Load the dataset from a local directory using pickle."""
-    with open(wandb.config.NEW_DATASET_NAME_LOCAL, "rb") as file:
+    filename = wandb.config.NEW_DATASET_NAME_LOCAL if filename is None else filename
+    with open(filename, "rb") as file:
         dataset = pickle.load(file)
     return dataset
 
@@ -65,4 +67,12 @@ def generate_dataset():
 
 
 if __name__ == "__main__":
-    load_json_to_dataframe()
+    dataset_name = "../../data/processed/process_documentation30.pkl"
+
+    dataset = load_json_to_dataframe()
+    store_dataset_locally(dataset=dataset, filename=dataset_name)
+    dataset = load_dataset_from_local(filename=dataset_name)
+
+    for i in range(5):
+        print(f"** Documentation Instruction ** \n{dataset.loc[i, 'documentationInstruction']}")
+        print(f"** Processed Documentation ** \n{dataset.loc[i, 'processedDocumentation']}\n\n")
